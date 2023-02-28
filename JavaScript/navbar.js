c4caee6a8bfc69cac4ba7b8776cc73e174bf5489
Richard S <119665239+Richard-Sen27@users.noreply.github.com>
2023-02-28 15:42:13 +0100

    Add files via upload

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,7 +1,7 @@
 document.getElementById('navbar').innerHTML = `
 
 <div class="container-fluid">
-    <a class="navbar-brand text-light" href="../HTML/homepage.html">Home</a>
+    <a class="navbar-brand text-light" href="../HTML/index.html">Home</a>
     <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse"
         data-bs-target="#collapsibleNavbar">
         <i class="bi bi-list text-white navbar-toggler-icon"></i>
@@ -12,7 +12,7 @@ document.getElementById('navbar').innerHTML = `
                 <a class="nav-link text-light link" href="#">Steuern</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-light link" href="#">Disskussionen</a>
+                <a class="nav-link text-light link" href="./Kommentare.html">Kommentare</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown">Infos</a>
