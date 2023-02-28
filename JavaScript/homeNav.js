c8d13260002f511346978b815ca6d4e0bb7802b6
Mojmir Horvath <75137860+LordMojmir@users.noreply.github.com>
2023-02-28 17:29:29 +0100

    Comments working with design

--- JavaScript/homeNav.js
+++ JavaScript/homeNav.js
@@ -1,7 +1,7 @@
 document.getElementById('navbar').innerHTML = `
 
 <div class="container-fluid">
-    <a class="navbar-brand text-light" href="../index.html">Home</a>
+    <a class="navbar-brand text-light" href="index.html">Home</a>
     <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse"
         data-bs-target="#collapsibleNavbar">
         <i class="bi bi-list text-white navbar-toggler-icon"></i>
@@ -12,15 +12,15 @@ document.getElementById('navbar').innerHTML = `
                 <a class="nav-link text-light link" href="#">Steuern</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-light link" href="./Kommentare.html">Kommentare</a>
+                <a class="nav-link text-light link" href="HTML/Kommentare.html">Kommentare</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown">Infos</a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="./Praktikum.html">Praktikum</a></li>
-                    <li><a class="dropdown-item" href="./Sommerjob.html">Sommerjob</a></li>
-                    <li><a class="dropdown-item" href="./Nebenjob.html">Nebenjob</a></li>
-                    <li><a class="dropdown-item" href="./Aushilfarbeit.html">Aushilfarbeiten</a></li>
+                    <li><a class="dropdown-item" href="HTML/Praktikum.html">Praktikum</a></li>
+                    <li><a class="dropdown-item" href="HTML/Sommerjob.html">Sommerjob</a></li>
+                    <li><a class="dropdown-item" href="HTML/Nebenjob.html">Nebenjob</a></li>
+                    <li><a class="dropdown-item" href="HTML/Aushilfarbeit.html">Aushilfarbeiten</a></li>
                 </ul>
             </li>
         </ul>
